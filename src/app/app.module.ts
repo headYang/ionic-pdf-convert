c805da4c88ad54b49b32267a477297e7d4b32105
jinyang <hirotohiroto@HirotodeiMac.local>
2018-07-09 11:43:03 +0800

    Fix app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { File } from '@ionic-native/file';
+import { FileOpener } from '@ionic-native/file-opener';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+   
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +27,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    File,
+    FileOpener
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,88 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { FileOpener } from '@ionic-native/file-opener';
+import { File } from '@ionic-native/file';
+import pdfMake from 'pdfmake/build/pdfmake.js';
+import pdfFonts from 'pdfmake/build/vfs_fonts.js';
 
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  letterObj = {
+    from: 'sim',
+    to: 'paul',
+    text: 'create pdf'
+  };
+  
+  pdfObj = null;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+      private plt: Platform,
+      private file: File,
+      private fileOpen: FileOpener
+    ) {
 
+  }
+
+  createPdf() {
+    var docDefinition = {
+      content: [
+        { text: 'Reminder', style: 'header' },
+        { text: new Date().toString(), alignment: 'right' },
+
+        { text: 'From', style: 'subheader' },
+        { text: this.letterObj.from },
+
+        { text: 'To', style: 'subheader' },
+        this.letterObj.to,
+
+        { text: this.letterObj.text, style: 'story', margin: [0, 20, 0, 20] },
+
+        {
+          ul: [
+            'bacon',
+            'rias',
+            'bbq',
+          ]
+        }
+      ],
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+        },
+        subheader: {
+          fontSize: 14,
+          bold: true,
+          margin: [0, 15, 0, 0]
+        },
+        story: {
+          italic: true,
+          alignment: 'center',
+          width: '50%'
+        }
+      }
+    }
+    this.pdfObj = pdfMake.createPdf(docDefinition);
+  }
+
+  downloadPdf() {
+    if(this.plt.is('cordova')) {
+      this.pdfObj.getBuffer((buffer) => {
+        var utf8 = new Uint8Array(buffer);
+        var binaryArray = utf8.buffer;
+        var blob = new Blob([binaryArray], { type: 'application/pdf' });
+
+        this.file.writeFile(this.file.dataDirectory, 'myletter.pdf', blob, { replace: true }).then(fileEntry => {
+          this.fileOpen.open(this.file.dataDirectory + 'myletter.pdf', 'application/pdf');
+        })
+      })
+    } else {
+      this.pdfObj.download();
+    }
   }
 
 }
